e4bac88ee3c2ca59e208f3838720a63aec8a1246
Stephen <stephen.ermshar@wallawalla.edu>
2017-10-15 09:53:25 -0700

    initial HW4 commit

---
+++ HW4/ermshar_stephen_hw4.asm
@@ -0,0 +1,18 @@
+# Stephen Ermshar
+# CPTR 280
+# HW 4
+# 2017 OCT 18
+
+# a) t3=t4+t5–t6;
+# b) s3 = t2/(s1 – 5);
+# c) sp=sp–16;
+# d) cout << t3;
+# e) cin >> t0;
+# f) a0 = &array;
+# g) t8 = mem(a0);
+# h) mem(a0 + 16) = 32768;
+# i) Cout << “Hello World”;
+# j) t0 = 2147483647 – 2147483648;
+# k) s0=-1*s0;
+# l) s1=s1*a0;
+# m) s2=s4*8;
