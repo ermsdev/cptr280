a6ccef1c4f178f2b3f7bb843cac2eb8363b69a7b
Stephen <stephen.ermshar@wallawalla.edu>
2017-10-15 14:29:29 -0700

    finished intial coding, untested, (g) and (h) not completed

--- HW4/ermshar_stephen_hw4.asm
+++ HW4/ermshar_stephen_hw4.asm
@@ -36,15 +36,38 @@
         move    $t0,    $v0
 
         # f) a0 = &array;
+        la      $a0,    array1          # load base address of array1 into $t0, not sure if that's what this is asking for
+
         # g) t8 = mem(a0);
+                                        # not sure what this is asking for
+
         # h) mem(a0 + 16) = 32768;
+                                        # not sure what this is asking for
+
         # i) Cout << “Hello World”;
+        li      $a0,    hello
+        li      $v0,    4               # syscall_4 = print_string_$a0
+        syscall
+
         # j) t0 = 2147483647 – 2147483648;
+        li      $t0,    2147483647
+        addi    $t0,    $t0,    -2147483648
+
         # k) s0=-1*s0;
+        li      $t0,    -1
+        mult    $t0,    $s0             # $t0 * $s0 = Hi and Lo registers
+        mflo    $s0                     # copy $Lo to $s0
+
         # l) s1=s1*a0;
+        mult    $a0,    $s1
+        mflo    $s1                     #copy $Lo to $s0
+
         # m) s2=s4*8;
+        li      $t0,    8
+        mult    $s4,    $t0             # $s4 * $t0 = Hi and Lo registers
+        mflo    $s2                     # copy $Lo to $s2
+
 
 .data
-    value:  .word 20,30,40
-    p4lb:   .word 20,105,4
-    p4ld:   .word 'a'
+    array1: .space  12                  # allocate 3 words (12 bytes)
+    hello:  .asciiz "Hello World"
