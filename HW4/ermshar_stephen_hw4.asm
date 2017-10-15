e3be8966f3b07e84f8cf921ca702cb666e01b16c
Stephen <stephen.ermshar@wallawalla.edu>
2017-10-15 15:22:08 -0700

    fixed parsing errors from qtSpim, runs fine, but I haven't checked that the output is correct

--- HW4/ermshar_stephen_hw4.asm
+++ HW4/ermshar_stephen_hw4.asm
@@ -18,7 +18,7 @@
         li      $t2,    105
         li      $t0,    5
         sub     $s3,    $s1,    $t0
-        div     $t2,    $s3             # how do div and mflo work?
+        div     $t2,    $s3                 # how do div and mflo work?
         mflo    $s4
 
         # c) sp=sp–16;
@@ -27,47 +27,51 @@
         # d) cout << t3;
         li      $t3,    'a'
         move    $a0,    $t3
-        li      $v0,    1               # syscall_1 = print_int_$a0
+        li      $v0,    1                   # syscall_1 = print_int_$a0
         syscall
 
         # e) cin >> t0;
-        li      $v0,    5               # syscall_1 = read_int
+        li      $v0,    5                   # syscall_1 = read_int
         syscall
         move    $t0,    $v0
 
         # f) a0 = &array;
-        la      $a0,    array1          # load base address of array1 into $t0, not sure if that's what this is asking for
+        la      $a0,    array_1             # load base address of array1 into $t0, not sure if that's what this is asking for
 
         # g) t8 = mem(a0);
-                                        # not sure what this is asking for
+                                            # not sure what this is asking for
 
         # h) mem(a0 + 16) = 32768;
-                                        # not sure what this is asking for
+                                            # not sure what this is asking for
 
         # i) Cout << “Hello World”;
-        li      $a0,    hello
-        li      $v0,    4               # syscall_4 = print_string_$a0
+        la      $a0,    hello_w
+        li      $v0,    4                   # syscall_4 = print_string_$a0
         syscall
 
         # j) t0 = 2147483647 – 2147483648;
-        li      $t0,    2147483647
-        addi    $t0,    $t0,    -2147483648
+        li      $t0,    2147483647          # highest integer that can be held in a single register
+        li      $t1,    -2147483648         # lowest integer that can be held in a single register
+        add     $t0,    $t0,    $t1
 
         # k) s0=-1*s0;
         li      $t0,    -1
-        mult    $t0,    $s0             # $t0 * $s0 = Hi and Lo registers
-        mflo    $s0                     # copy $Lo to $s0
+        mult    $t0,    $s0                 # $t0 * $s0 = Hi and Lo registers
+        mflo    $s0                         # copy $Lo to $s0
 
         # l) s1=s1*a0;
         mult    $a0,    $s1
-        mflo    $s1                     #copy $Lo to $s0
+        mflo    $s1                         #copy $Lo to $s0
 
         # m) s2=s4*8;
         li      $t0,    8
-        mult    $s4,    $t0             # $s4 * $t0 = Hi and Lo registers
-        mflo    $s2                     # copy $Lo to $s2
+        mult    $s4,    $t0                 # $s4 * $t0 = Hi and Lo registers
+        mflo    $s2                         # copy $Lo to $s2
 
+        # exit program
+        li      $v0     10
+        syscall
 
 .data
-    array1: .space  12                  # allocate 3 words (12 bytes)
-    hello:  .asciiz "Hello World"
+    array_1:    .space  12                  # allocate 3 words (12 bytes) for array in (f)
+    hello_w:    .asciiz "Hello World"
