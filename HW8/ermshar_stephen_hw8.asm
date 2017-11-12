50a19ae800444784739d347f5b669d313fb11885
Stephen <stephen.ermshar@wallawalla.edu>
2017-11-12 13:39:00 -0800

    can now list numbers and the sums of their devisors

--- HW8/ermshar_stephen_hw8.asm
+++ HW8/ermshar_stephen_hw8.asm
@@ -16,29 +16,103 @@
 
 .data
 prompt:		.asciiz		"Enter 1 to begin computation, 0 to quit: "
-prompt2:	.asciiz		"sum of proper devisors of 284: "
-space:		.asciiz		", "
+space:		.asciiz		" "
+nl:			.asciiz		"\n"
 
 .text
 main:
 	# t0 --> artificial immediate (for instructions that don't support immediate values)
-	la		$a0,	prompt
-	li		$v0,	4				# syscall_4: print string
+
+#--------------------------------------------------------------------
+# welcome message: prompts beginning of computation
+# BEGIN
+#--------------------------------------------------------------------
+	# la		$a0,	prompt
+	# li		$v0,	4				# syscall_4: print string
+	# syscall
+
+	# li		$v0,	5				# syscall_5: read int
+	# syscall
+
+	# beq		$v0,	$0,		end		# end if user enters 0
+	# li		$t0,	1				# $t1 = 1
+	# bne		$v0,	$t0,	main	# start over if user doesn't enter a 1 (or a 0 as a result of last instruction)
+#--------------------------------------------------------------------
+# END
+# welcome message: prompts beginning of computation
+#--------------------------------------------------------------------
+
+# 	li		$t1,	219
+
+# search_n:
+# 	addi	$t1,	$t1,	1
+# 	move 	$a3,	$t1
+# 	jal		sum_divs
+# 	nop
+# 	move 	$t2,	$v1
+# 	move	$a3,	$t2
+# 	jal		sum_divs
+# 	nop
+# 	move 	$t3,	$v1
+# 	beq		$t1, $t3, print_pair
+# 	nop
+# 	j		search_n				# jump to search_n
+# 	nop
+
+
+# print_pair:
+# 	move	$a0,	$t1
+# 	li		$v0,	1				# syscall_4: print string
+# 	syscall
+
+# 	la		$a0,		space
+# 	li		$v0,		4				# syscall_4: print string
+# 	syscall
+
+# 	move	$a0,	$t2
+# 	li		$v0,	1				# syscall_4: print string
+# 	syscall
+
+# 	jr		$ra				# jump to ra, allows register jumps from branch instructions
+# 	nop
+
+	li		$a3,	1
+list_nums:
+	addi	$a3,	$a3,	1
+
+	move	$a0,	$a3
+	li		$v0,	1
 	syscall
 
-	li		$v0,	5				# syscall_5: read int
+	la		$a0,		space
+	li		$v0,		4				# syscall_4: print string
 	syscall
 
-	beq		$v0,	$0,		end		# end if user enters 0
-	li		$t0,	1				# $t1 = 1
-	bne		$v0,	$t0,	main	# start over if user doesn't enter a 1 (or a 0 as a result of last instruction)
+	jal		sum_divs
+	nop
 
-	la		$a0,		prompt2
-	li		$v0,		4			# syscall_4: print string
+	move 	$a0,	$v1
+	li		$v0,		1
 	syscall
 
-	li		$a3,	284
+	la		$a0,		nl
+	li		$v0,		4				# syscall_4: print string
+	syscall
 
+	j	list_nums
+	nop
+
+
+
+
+jal		sum_divs				# jump to sum_divs and save position to $ra
+nop
+
+move	$a0,	$v1
+li		$v0,	1
+syscall
+
+j		end				# jump to end
 
 #--------------------------------------------------------------------
 # sum_divs subroutine: input $a3, sums all proper devisors of input
@@ -55,35 +129,30 @@ sum_divs:
 check_sum_divs:
 	addi	$t8, $t8, 1			# 8 = $t1 + 0
 	div		$t9, $t8			# t9 / t8
-	mfhi	$t3					# $t3 = 9 mod $t1 
-	mflo	$t2					# $t2 = floor(9 / $t1) 
-	bne		$t3, $0, check_sum_divs
+	mfhi	$t6					# $t6 = 9 mod $t1 
+	mflo	$t5					# $t5 = floor(9 / $t1) 
+	bne		$t6, $0, check_sum_divs
 	nop
-	blt		$t2, $t8, sum_out	# if $t2 < $t8 then check_sum_divs
+	blt		$t5, $t8, sum_out	# if $t5 < $t8 then check_sum_divs
 	nop
-	add		$t7, $t7, $t8		# $t0 = $t1 + $t2
-	beq		$t2, $t8, check_sum_divs	# if 2 == $t1 then target
+	add		$t7, $t7, $t8		# $t0 = $t1 + $t5
+	beq		$t5, $t8, check_sum_divs	# if 2 == $t1 then target
 	nop
-	add		$t7, $t7, $t2
+	add		$t7, $t7, $t5
 	j		check_sum_divs				# jump to target
 	nop
 
 sum_out:
 	move	$v1,	$t7
+	# j		end
+	# nop
 	jr		$ra				# jump to ra, allows register jumps from branch instructions
-
+	nop
 #--------------------------------------------------------------------
 # END
 # sum_divs subroutine: input $a3, sums all proper devisors of input
 #--------------------------------------------------------------------
 
-
-
-
 end:
-	move	$a0,	$t7
-	li		$v0,	1				# syscall_4: print string
-	syscall
-
 	li		$v0,	10				# syscall_10: terminate program
 	syscall
