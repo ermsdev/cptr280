54ecfb6217761fe147667490082298f0fff966c9
Stephen <stephen.ermshar@wallawalla.edu>
2017-11-15 10:50:58 -0800

    incomplete implementation of frame pointers

--- HW9/ermshar_stephen_hw9.asm
+++ HW9/ermshar_stephen_hw9.asm
@@ -20,6 +20,7 @@
 	bye:        .asciiz     "Program Ended"
 
 .text
+.globl main
 	main:
 		la		$a0,	prompt
 		li		$v0,	4				# syscall_4: print string
@@ -28,11 +29,41 @@
 		syscall
 		move	$s0,	$v0
 		blt		$s0,	$0,		end		# end if user enters negative number
+		nop
 
-		move	$a0,	$s0	
+		move	$a0,	$s0
+		jal		factorial
+		nop
+
 	factorial:
-		move	$t0,	$a0
-		
+		addi	$sp,	$sp, -12	# adjust stack for 3 items
+		sw		$ra,	8($sp)		# save the return address
+		sw		$fp,	4($sp)		# save the frame pointer
+		sw		$a0,	0($sp)		# save the argument n
+		move	$fp,	$sp			# initialize the frame pointer
+
+		slti 	$t0,	$a0,	1	# test for n < 1
+		beq		$t0,	$0,		L1	# if n >= 1, go to L1
+		nop
+
+		addi	$v0,	$0, 	1	# if n < 1, return 1
+		addi	$sp,	$sp,	12	# pop 3 items off stack
+		jr		$ra
+		nop
+
+	L1:
+		addi	$a0,	$a0,	-1
+		jal 	factorial
+		nop
+
+		lw		$a0,	0($sp)		# return from jal: restore argument n
+		lw		$ra,	4($sp)		# restore the return address
+		addi	$sp,	$sp,	12	# adjust stack pointer to pop 3 items
+
+		mul		$v0,	$a0,	$v0	# return n * fact(n-1)
+
+		jr $ra						# return to the caller
+		nop
 
 	end:
 		la		$a0,		bye
