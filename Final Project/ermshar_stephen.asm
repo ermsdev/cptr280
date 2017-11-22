ce3c43c99b6b1909f551e4ac716477044728928f
Stephen <stephen.ermshar@wallawalla.edu>
2017-11-21 21:11:11 -0800

    fixed search demo by making all opcode strings the same length

--- Final Project/ermshar_stephen.asm
+++ Final Project/ermshar_stephen.asm
@@ -10,7 +10,7 @@
 # TODO: try pre-sorting these and using a binary search
 # NOTE: many instructions share a common initial 6 bits, like sycall and sll (000000) and differ in the last 6 bits.
     opc:        .byte   0x20, 0x21, 0x08, 0x09, 0x24, 0x0C, 0x1A, 0x1B, 0x18, 0x19, 0x27, 0x25, 0x0D, 0x00, 0x04, 0x03, 0x07, 0x02, 0x06, 0x22, 0x23, 0x26, 0x0E, 0x19, 0x18, 0x2A, 0x29, 0x0A, 0x09, 0x04, 0x07, 0x06, 0x05, 0x02, 0x03, 0x09, 0x08, 0x20, 0x24, 0x21, 0x25, 0x23, 0x28, 0x29, 0x2B, 0x10, 0x12, 0x11, 0x13, 0x1A
-    opc_txt:    .asciiz "add", "addu", "addi", "addiu", "and", "andi", "div", "divu", "mult", "multu", "nor", "or", "ori", "sll", "sllv", "sra", "srav", "srl", "srlv", "sub", "subu", "xor", "xori", "lhi", "llo", "slt", "sltu", "slti", "sltiu", "beq", "bgtz", "blez", "bne", "j", "jal", "jalr", "jr", "lb", "lbu", "lh", "lhu", "lw", "sb", "sh", "sw", "mfhi", "mflo", "mthi", "mtlo", "trap"
+    opc_txt:    .asciiz "add    ", "addu   ", "addi   ", "addiu  ", "and    ", "andi   ", "div    ", "divu   ", "mult   ", "multu  ", "nor    ", "or     ", "ori    ", "sll    ", "sllv   ", "sra   ", "srav   ", "srl    ", "srlv   ", "sub    ", "subu   ", "xor    ", "xori   ", "lhi    ", "llo    ", "slt    ", "sltu    ", "slti    ", "sltiu  ", "beq    ", "bgtz   ", "blez   ", "bne    ", "j      ", "jal    ", "jalr   ", "jr     ", "lb     ", "lbu    ", "lh     ", "lhu    ", "lw     ", "sb     ", "sh     ", "sw     ", "mfhi   ", "mflo   ", "mthi   ", "mtlo   ", "trap   "
 
     prompt:     .asciiz "enter the integer decimal representation of an opcode: "
     prompt2:    .asciiz "success"
@@ -21,13 +21,9 @@
         li		$v0,	4				# syscall_4: print string
         syscall
 
-        # li		$v0,	5				# syscall_5: read int
-        # syscall 
-        # move 	$s0,    $v0             # save input to $s0
-
-        ### test user input
-        li      $s0,    32
-        ###
+        li		$v0,	5				# syscall_5: read int
+        syscall 
+        move 	$s0,    $v0             # save input to $s0
 
         # base address of opcode bitpatterns
         la      $s1,    opc
@@ -44,12 +40,15 @@
         nop
         # bne     $s3, $s0, n_found
         # nop
-        addi    $s3,    $s2,    1   # otherwise increment the offset and...
+        addi    $s3,    $s3,    1   # otherwise increment the offset and...
         j		search              # check again
         
 
     found:
-        add     $a0,    $s3,    $s2
+        li      $t0,    8
+        mult    $s3,    $t0
+        mflo    $s3
+        add     $a0,    $s3,    $s2     # add offset * 2 and opcode string base address
         li		$v0,	4				# syscall_4: print string
         syscall
 
