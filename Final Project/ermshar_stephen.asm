64cfe47ee4014e9d069be9a9898fd151571b2b8f
Stephen <stephen.ermshar@wallawalla.edu>
2017-11-21 19:19:52 -0800

    added list of opcodes to .data

--- Final Project/ermshar_stephen.asm
+++ Final Project/ermshar_stephen.asm
@@ -6,11 +6,18 @@
 
 .data
 # http://alumni.cs.ucr.edu/~vladimir/cs161/mips.html
-    opc:    .byte   10011001, 10011001, 10011001, 10011001
-
+# used a spreadsheet and textwrangler to turn the table in the link into these two lines
+# TODO: try pre-sorting these and using a binary search
+    opc:        .byte   0x20, 0x21, 0x08, 0x09, 0x24, 0x0C, 0x1A, 0x1B, 0x18, 0x19, 0x27, 0x25, 0x0D, 0x00, 0x04, 0x03, 0x07, 0x02, 0x06, 0x22, 0x23, 0x26, 0x0E, 0x19, 0x18, 0x2A, 0x29, 0x0A, 0x09, 0x04, 0x07, 0x06, 0x05, 0x02, 0x03, 0x09, 0x08, 0x20, 0x24, 0x21, 0x25, 0x23, 0x28, 0x29, 0x2B, 0x10, 0x12, 0x11, 0x13, 0x1A
+    opc_txt:    .asciiz "add", "addu", "addi", "addiu", "and", "andi", "div", "divu", "mult", "multu", "nor", "or", "ori", "sll", "sllv", "sra", "srav", "srl", "srlv", "sub", "subu", "xor", "xori", "lhi", "llo", "slt", "sltu", "slti", "sltiu", "beq", "bgtz", "blez", "bne", "j", "jal", "jalr", "jr", "lb", "lbu", "lh", "lhu", "lw", "sb", "sh", "sw", "mfhi", "mflo", "mthi", "mtlo", "trap"
 
 .text
     main:
+        la		$t1, opc		# 
+        lw		$t1, 0($t1)		# 
+        
+        
+
 # use shifts and bools to compare non-byte aligned patterns
 #   get first 8 bits (byte) of instruction
 #   shift right logical so the last 6 bits of selected byte are the opcode
