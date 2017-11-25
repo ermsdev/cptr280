3b679ee584c08f2831a19140b87492350556a5d4
Stephen <stephen.ermshar@wallawalla.edu>
2017-11-24 18:34:40 -0800

    check if last no longer takes on the reponsibility of calling end, but it passes that onto its caller. setting up opcodes_and_hex to be more organized with respect to duplicate opcodes

--- Final Project/get_inst_2.asm
+++ Final Project/get_inst_2.asm
@@ -18,7 +18,9 @@
 
 ### MAIN LOOP
 	main_loop:
-		jal		get_inst					# get the instruction
+		add		$s7,	$0,		$0			# set s7 to 0 in case it got messed with
+
+		jal		get_inst					# get the instruction and PUT IT IN $s1
 		nop
 
 		jal 	check_if_last				# check if it's the last instruction, print it and quit if it is
@@ -28,12 +30,14 @@
 		nop
 
 		# jal		print_opc
-		nop
+		# nop
 
 		la		$a0,	nl
 		li		$v0,	4				# syscall_4: print string newline
 		syscall
 
+		bne		$s7,	$0,		end		# a 1 in s7 indicates that this was the last loop DO NOT USE s7 WITHOUT PUTTING IT BACK!
+		
 		addi	$s0,	$s0,	4		# go to next instruction
 		j main_loop						# loop again
 		nop
@@ -52,20 +56,27 @@
 		#		0x10 : check 5 bits in 2nd field
 		#		all other 6 bits opcodes are non-duplicates and can be checked directly
 
+		# branch to get_opc
+		# branch depending on case
+		# return ascii value address
 
 ### ------
 
-## CHECK IF LAST : checks if the current instruction is the last one, if it is it prints it and jumps to end
+### GET OPC : 
+	get_opc:
+
+### ------
+
+### CHECK IF LAST : checks if the current instruction is the last one, if it is it sets s7 to 1, otherwise it returns without changing anythin
 	check_if_last:
 		li		$t0,	0xC				# immediate register : syscall bitpattern
 		bne		$s1,	$t0,	not_last_out
+		nop
 		li		$t0,	0x2402000A		# immediate register : instruction that would have loaded 10 into $v0
 		lw		$t1,	-4($s0)			# load the last instruction into $t1
 		bne		$t0,	$t1,	not_last_out	# if the last instruction is not the expected one that would load 10 into $v0, then this is not the end
-		jal		print_hex_inst			# if it passes the first two branches then it's reached the end, print and quit
 		nop
-		j		end						
-		nop
+		li		$s7,	1				# a 1 in s7 will indicate to the program that this is the last instruction
 	not_last_out:
 		jr		$ra						# jump to $ra
 		nop
